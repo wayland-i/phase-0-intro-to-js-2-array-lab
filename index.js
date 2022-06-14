ff3ddbb892bced585a168bee623b088f703cf25a
Ian Iversen <ianiversen1@gmail.com>
2022-06-13 23:50:16 -0400

    commit tree 93dc036a3f4c409858148d646faeae018ea1aaff

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(name){
+    const moreCats = [...cats, name]
+    return moreCats
+}
+
+function prependCat(name){
+    return [name, ...cats]
+}
+
+function removeLastCat(){
+    return cats.slice(0, -1)
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
